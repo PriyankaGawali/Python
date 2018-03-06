5b685ddb46530a4341d6ff747d2fb1c256c4deaf
PriyankaGawali <37065008+PriyankaGawali@users.noreply.github.com>
2018-03-06 12:10:53 +0530

    Add files via upload

---
+++ Basic/String/is_rotation.py
@@ -0,0 +1,23 @@
+# WAP to accept 2 strings from user. And check if second string is a right rotation of first string.
+# Hint:
+# India, iaInd -> This should be true
+# Jeetendra 1st right rotation "aJeetendr"
+
+
+def is_rotation(input_str,validate_str):
+    if len(input_str) == len(validate_str):
+        return validate_str in input_str + input_str
+    return False
+
+
+def main():
+    input_str = input("Enter string:")
+    validate_str = input("Enter validate string:")
+    if is_rotation(input_str,validate_str):
+        print("{} is right rotation of {}".format(validate_str, input_str))
+    else:
+        print("{} is not right rotation of {}".format(validate_str, input_str))
+
+
+if __name__ == '__main__':
+    main()
