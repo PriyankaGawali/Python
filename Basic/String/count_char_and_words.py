4015079c2bcb83eb2208bda89ece8236696090c6
PriyankaGawali <37065008+PriyankaGawali@users.noreply.github.com>
2018-03-06 12:17:16 +0530

    Add files via upload

---
+++ Basic/String/count_char_and_words.py
@@ -0,0 +1,22 @@
+# WAP to accept a sentence from user and print count of characters , words in the input statement
+
+
+def count_char_words(string):
+    char_cnt = 0
+    word_cnt = 1
+    for ch in string:
+        char_cnt = char_cnt + 1
+        if ch == ' ' or ch == '\t':
+            word_cnt = word_cnt + 1
+
+    print("Number of characters : ", char_cnt)
+    print("Number of words : ", word_cnt)
+
+
+def main():
+    sentence = input("Enter sentence to count characters and words:")
+    count_char_words(sentence)
+
+
+if __name__ == '__main__':
+    main()
