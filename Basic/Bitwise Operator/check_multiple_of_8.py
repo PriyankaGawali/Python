d32939fe670f209838e0da860152ea34cb85a7b1
PriyankaGawali <37065008+PriyankaGawali@users.noreply.github.com>
2018-03-06 13:00:25 +0530

    Update check_multiple_of_8.py

--- Basic/Bitwise Operator/check_multiple_of_8.py
+++ Basic/Bitwise Operator/check_multiple_of_8.py
@@ -1,18 +1,7 @@
-# Write a program to accept number from user and check if it is multiple
+''' Write a program to accept number from user and check if it is multiple
 # of 8 (without using any arithmetic operators).
 # Hint: If last 3 bits are 0, then it is multiple of 8.
-# If any of the last 3 bits is not 0, then it is not multiple of 8.
-# Also, we need to & with (2^3 - 1). Because we need 111, and logic is
-# if we want all 1s then (2^no.-1) will give those many 1s.
-
-# Pattern:
-# 8    ->      1 000
-# 16    ->    10 000
-# 24    ->    11 000
-# 48    ->   110 000
-# 56    ->   111 000
-# 64    ->  1000 000
-
+'''
 
 def multiple_of_8(no):
     return no & ((2 ^ 3) - 1) == 0
