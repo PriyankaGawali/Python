dda2345948acfce1715f01f9771f79c9adc07104
PriyankaGawali <37065008+PriyankaGawali@users.noreply.github.com>
2018-03-12 16:15:31 +0530

    Create pattern_1.py

---
+++ Basic/Pattern/pattern_1.py
@@ -0,0 +1,21 @@
+'''Write a program to display pattern
+*
+*  *
+*  *   *
+*  *   *   *
+'''
+
+def print_pattern(n):
+    for i in range(1, n + 1):
+        for j in range(i):
+            print("*", end="  ")
+        print("")
+
+
+def main():
+    row = int(input("Enter no. of rows:"))
+    print_pattern(row)
+
+
+if __name__ == '__main__':
+    main()
